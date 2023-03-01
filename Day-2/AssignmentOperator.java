4ffd781679105ed99d56f3a78c19579de7c4ca49
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-02 00:16:20 +0530

    Day-2 programs done

---
+++ Day-2/AssignmentOperator.java
@@ -0,0 +1,15 @@
+class AssignmentOperator{
+ public static void main(String[] args){
+   int a=10,b=20,c=5,d=2,e=2;
+  System.out.println(a+=5);
+  System.out.println(b*=10);
+  System.out.println(c/=2);
+  System.out.println(d%=1);
+  System.out.println(e-=1);
+  System.out.println(a&=3);
+  System.out.println(a!=3);
+  System.out.println(a^3);
+  System.out.println(a>>=3);
+  System.out.println(a<<=2);
+  }
+ }
---
+++ Day-2/ShiftOperator.java
@@ -0,0 +1,7 @@
+class ShiftOperator{
+ public static void main(String[] args){
+  int a=20;
+  System.out.println(a>>2);//right shift operator
+  System.out.println(a<<1);//left shift operator
+  }
+ }
