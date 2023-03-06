6258b50b2b6db4e5fbb4d6f99420942d239a9cd2
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-06 17:30:41 +0530

    Day-6 programs done

--- Day-6/Number_Pattern15.java
+++ Day-6/Number_Pattern15.java
@@ -21,10 +21,12 @@ class Number_Pattern15{
         System.out.print(j);
         System.out.println();
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<n;j++)
-        System.out.print(n-i-1);
-        for(int k=n-1;k<=n;k++)
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=(n-i-1);j++)
+        System.out.print(" ");
+       /* for(int k=n-1;k<=n;k++)
+        System.out.print(k);*/
+        for(int k=(n-i);k<=n;k++)
         System.out.print(k);
         System.out.println();
     }
