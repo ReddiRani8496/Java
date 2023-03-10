75ded6a0eb4d3384ab9e92082fb0c7cbc5008b3e
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-10 22:05:25 +0530

    Day-12 programs done

--- Day-12/Sorting_Zeroes.java
+++ Day-12/Sorting_Zeroes.java
@@ -3,18 +3,16 @@ import java.util.Arrays;
 class Sorting_Zeroes{
     public static void main(String[] args){
         int arr[]={16,0,4,2,0,8,5,0};
-        int len=arr.length;
-    
-        for(int i=len-1;i>=0;i--){
-            if(arr[i]!=0)
-            for(int j=0;j<i;j++){
-            if(arr[j]==0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                
-            }
-        }}
-        System.out.println(Arrays.toString(arr));
+       int nonZero=0;
+       int n=arr.length;
+       for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            int temp=arr[i];
+            arr[i]=arr[nonZero];
+            arr[nonZero]=temp;
+            nonZero++;
+        }
+       }
+       System.out.println(Arrays.toString(arr));
     }
 }
