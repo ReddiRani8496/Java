e1ce0c8fc203631917c9f684c55a316aa55f8d94
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-11 21:50:03 +0530

    Day-13 programs done

--- Day-13/FirstRepeatingElement.java
+++ Day-13/FirstRepeatingElement.java
@@ -3,12 +3,16 @@ class FirstRepeatingElement{
     public static void main(String[] args){
         int arr[]={3,4,5,4,6,5};
         int len=arr.length;
+        
         for(int i=0;i<len;i++){
-            for(int j=i+1;j<len;j++)
-             if(arr[j]==arr[i]){
+         for(int j=i+1;j<len;j++){
+            if(arr[j]==arr[i]){
                 System.out.println(arr[i]);
                 return;
-             }
+            }
+         }
+          
         }
+
     }
 }
