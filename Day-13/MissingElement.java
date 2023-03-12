133f5678e0753bc3381b3a1e3cf0c7174bd4d7ba
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-12 13:05:58 +0530

    Day-13 programs done

---
+++ Day-13/MissingElement.java
@@ -0,0 +1,17 @@
+class MissingElement{
+   public static void main(String[] args){
+       int arr[]={13,14,12,15};
+        int n=arr.length;
+         int min=11;
+        int max=15;
+      int maxSum=max*(max+1)/2;
+       int minSum=(min-1)*(min)/2;
+       int expectedSum=maxSum-minSum;
+        int actualSum=0;
+        for(int i=0;i<n;i++)
+        actualSum+=arr[i];
+        System.out.println(expectedSum-actualSum);
+       
+    }
+    
+}
