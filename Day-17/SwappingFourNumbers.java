61e713d677c2aab760e4c7362ddca15ae92af959
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-15 21:46:59 +0530

    Day-17 programs done

--- Day-17/SwappingFourNumbers.java
+++ Day-17/SwappingFourNumbers.java
@@ -7,11 +7,11 @@ class SwappingFourNumbers{
     public static void main(String[] args){
         int a=10,b=20,c=30,d=40;
         System.out.println("Before swapping a="+a+" b="+b+" c="+c+" d="+d);
-        a=a+b+c+d;
-        b=a-b-c-d;
-        c=a-b-c-d;
-        d=a-b-c-d;
-        a=a-b-c-d;
+        d=a+b+c+d;
+        a=d-a-b-c;
+        b=d-b-c-a;
+        c=d-a-b-c;
+        d=d-a-b-c;
         System.out.println("After swapping a="+a+" b="+b+" c="+c+" d="+d);
     }
 }
--- Day-17/SwappingThreeNumbers.java
+++ Day-17/SwappingThreeNumbers.java
@@ -1,16 +1,16 @@
 /*program to swap three numbers
 a=10 b=20 c=30
 after swapping
-a=30 b=10 c=20
+a=20 b=30 c=10
 */
 class SwappingThreeNumbers{
     public static void main(String[] args){
         int a=10,b=20,c=30;
         System.out.println("Before swapping a="+a+" b="+b+" c="+c);
-        a=a+b+c;
-        b=a-b-c;
-        c=a-b-c;
-        a=a-b-c;
+        b=a+b+c;
+        a=b-a-c;
+        c=b-a-c;
+        b=b-a-c;
         System.out.println("After swapping a="+a+" b="+b+" c="+c);
     }
 }
