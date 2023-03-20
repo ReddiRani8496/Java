83ba5f86a9433995a9ad84630fd522083cf9d50c
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-20 22:19:05 +0530

    Day-22 programs done

--- Day-22/MaxDiff.java
+++ Day-22/MaxDiff.java
@@ -3,15 +3,17 @@ class MaxDiff{
     public static void main(String[] args){
     int arr[]={6,3,8,7,2,9,4};
     int len=arr.length;
-    int smallIndex=0;
+    int smallIndex=arr[0];
     int maxIndex=0;
     for(int i=0;i<len;i++){
-        if(arr[i]<arr[smallIndex])
-        smallIndex=i;
-        int diff=smallIndex-maxIndex;
+        if(arr[i]<smallIndex){
+        smallIndex=arr[i];
+        maxIndex=0;
+        }
+        int diff=arr[i]-smallIndex;
         if(diff>maxIndex)
          maxIndex=diff;
     }
-    System.out.println(arr[maxIndex]);
+    System.out.println(maxIndex);
 }
 }
