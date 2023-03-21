c3e9ac49fefeebcab3e4c5342048ef74d4d60615
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-21 20:31:56 +0530

    Day-21 programs done

--- Day-21/TrappingRainWater.java
+++ Day-21/TrappingRainWater.java
@@ -3,7 +3,7 @@ class TrappingRainWater{
         int arr[]={0,1,0,2,1,0,1,3,2,1,2,1};
         int water=0;
         int len=arr.length;
-        for(int i=0;i<len-1;i++){
+        for(int i=1;i<len-1;i++){
             int leftMax=arr[i];
             for(int j=0;j<i;j++){
                 if(arr[j]>leftMax)
