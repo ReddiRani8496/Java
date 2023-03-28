9eaf62ccedf040e122c3485be87c34b6ecd332c4
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-28 23:35:02 +0530

    Day-29 programs done

--- Day-29/BracketsComparision.java
+++ Day-29/BracketsComparision.java
@@ -1,6 +1,6 @@
 class BracketsComparision{
     public static void main(String[] args){
-        String str="()))[]";
+        String str="()())[][";
         int opens=0;
         int closes=0;
         int mismatch=0;
@@ -10,31 +10,34 @@ class BracketsComparision{
             else if(str.charAt(i)==')'){
                 if(opens>0){
                 opens--;
-                closes++;
-            }
-                else{
-                 mismatch++;
-                
                 }
-                 if(closes>0)
-                 closes--;
-            }
-        }
-        for(int i=0;i<str.length();i++){
+                
+            else{
+                 mismatch++;
+                 closes++;
+                }
+              
+                  }
+                
+                 
+                }
+                 if(opens>=1)
+                  mismatch++;
+            for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='[')
             opens++;
             else if(str.charAt(i)==']'){
                 if(opens>0){
                 opens--;
-                closes++;
             }
                 else{
                  mismatch++;
-                 if(closes>0)
-                 closes--;
+                  closes++;
                  }
             }
-        }
-        System.out.println("opens="+opens+"closes="+closes+"mismatch="+mismatch);
+          }
+         if(opens>=1)
+                  mismatch++;
+        System.out.println("opens="+opens+"\tcloses="+closes+"\tmismatch="+mismatch);
     }
 }
