72fc8a4f3ab30abcde22085b0d0351e0b6b0fd55
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-30 23:44:03 +0530

    DAy-20 programs done ;

--- Day-20/MergingTwoArrays.java
+++ Day-20/MergingTwoArrays.java
@@ -3,8 +3,7 @@ class MergingTwoArrays{
     public static void main(String[] args){
         int a[]={1,2,3,4,9};
         int b[]={5,6,7,8};
-        int max=a.length>b.length?a.length:b.length;
-        int c[]=new int[a.length+b.length];
+       int c[]=new int[a.length+b.length];
         int i,j,k;
         for(i=0,j=0,k=0;i<a.length&&j<b.length;){
             if(a[i]<b[j])
---
+++ Day-20/StringConcat.java
@@ -0,0 +1,13 @@
+/*program to concat two strings
+str1="java"
+str2="world"
+str3="java world"
+*/
+class StringConcat{
+    public static void main(String[] args){
+        String str1="java ";
+        String str2="world";
+        String str3=str1.concat(str2);
+        System.out.println(str1.concat(str2));
+    }
+}
