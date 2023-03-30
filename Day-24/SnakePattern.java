c473ea682e2b962ee7de9479480b295e158defe8
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-30 23:43:39 +0530

    Day-24 programs done

--- Day-24/SnakePattern.java
+++ Day-24/SnakePattern.java
@@ -5,12 +5,17 @@ class SnakePattern{
                      {7,8,9}};
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                if(j==0){
+                if(j%2==0){
                     System.out.print(arr[i][j]+" ");
                     
                 }
             }
-            System.out.println();
+            
+        }
+        for(int i=arr.length;i<arr.length-1;i++){
+            for(int j=1;j<arr.length;j++){
+                System.out.print(arr[i][j]);
+            }
         }
     }
 }
