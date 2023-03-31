a6f87e0335869712a506d93557d481a3266fc5da
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-03-31 10:03:27 +0530

    Day-31 programs done

--- Day-31/BunnyEars.java
+++ Day-31/BunnyEars.java
@@ -1,15 +1,13 @@
 //program to find out number of ears across all the bunnies recursively, where each bunny has 2 ears
 class BunnyEars{
     public static void main(String[] args){
-        int bunnies=1;
+        int bunnies=3;
        int total= bunnyEars( bunnies);
        System.out.println(total);
     }
     static int bunnyEars(int bunnies){
         if(bunnies==0)
         return 0;
-        if(bunnies==1)
-        return 1;
-        return 2*bunnies;
+        return 2+bunnyEars(bunnies-1);
     }
 }
