ba4a675c96a4c4cbbc70c039937af2ecb34774ca
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-04-05 23:00:48 +0530

    Day-36 programs done

---
+++ Day-36/InsertionSort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class InsertionSort{
+    public static void main(String[] args){
+        int arr[]={4,2,7,1};
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
