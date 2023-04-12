1c048c75b5ec42cd791fe21819fc24610c99d983
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-04-12 23:40:21 +0530

    Day-43 programs done

---
+++ Day-43/Vehicle.java
@@ -0,0 +1,15 @@
+class Vehicle{
+    public static void main(String[] args) {
+        Cars.type();
+    }
+}
+class Cars{
+    int x=10;
+    static void type(){
+        System.out.println("This is a vehicle");
+    }
+
+class Audi {
+  //System.out.println(x)
+}
+}
