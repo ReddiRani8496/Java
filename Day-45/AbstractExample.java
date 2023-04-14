34215e4e4810e9a2ee4a4f23be8b457d88f82dd0
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-04-14 14:31:14 +0530

    Day-45 programs done

---
+++ Day-45/AbstractExample.java
@@ -0,0 +1,34 @@
+/*
+ * The class which is declared as abstract is called abstract class
+ * if one method is abstract then class must be declared as abstract, abstact methods do not specify a body.
+ * we can declare class as abstract, alothough there are no abstract methods
+ */
+class AbstractExample{
+    public static void main(String[] args) {
+       SBI s=new SBI(); 
+     s.deposit();
+     s.intrestRate();
+    UnionBank u=new UnionBank();
+     u.intrestRate();
+    }
+}
+abstract class RBI{
+    void deposit(){
+        System.out.println("Amount deposited into bank");
+    }
+    void withdraw(){
+        System.out.println("money is withdrawing");
+    }
+  abstract  void intrestRate();
+}
+class SBI extends RBI{
+   @Override
+   void intrestRate() {
+       System.out.println("10%");
+   }
+}
+class UnionBank extends RBI{
+    void intrestRate(){
+        System.out.println("12%");
+    }
+}
--- practice/average.java
+++
@@ -1,9 +0,0 @@
-class average{
-    public static void main(String[] args){
-        int a=21;
-        int b=30;
-        double c=(a+b)/2;
-        System.out.println(c);
-    }
-}
-
