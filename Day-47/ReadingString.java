81d3d48ebada20a718f10776443d86dc4e570986
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-04-16 21:42:50 +0530

    Day-47 programs done

---
+++ Day-47/ReadingString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ReadingString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the sentence:");
+        String hello=sc.nextLine();
+        System.out.println(hello);
+        sc.close();
+    }
+}
