7a6a4dd088985e6863eee3d86c11f46de504bb74
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-04-26 07:03:13 +0530

    DAy-54 programs done

---
+++ Day-54/Even.java
@@ -0,0 +1,15 @@
+public class Even {
+    public static void main(String[] args) {
+        for(var x : xyz){
+            int num=Integer.parseInt(x);
+            System.out.println(x);
+            int rem=num%2;
+            if(rem==0)
+                System.out.println("Even");
+            else   
+                System.out.println("Odd");
+            
+        }
+    }
+    
+}
---
+++ Day-54/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int sum=0;
+        for(var x : xyz){
+            sum += Integer.parseInt(x);
+        }
+        System.out.println(sum);
+    }
+    
+}
