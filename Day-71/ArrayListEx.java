6dc16c32230101d3f6c3f80adede6419369d87ca
Harikabs <bsharika379@gmail.com>
2023-05-11 20:32:50 +0530

    Day-71 programs done

---
+++ Day-71/ArrayListEx.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(110);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+        arr.add(60);
+        arr.add(70);
+        arr.add(80);
+        arr.add(90);
+        arr.add(100);
+        System.out.println(arr.get(3));
+        Collections.sort(arr);
+        System.out.println(arr);
+    }
+}
