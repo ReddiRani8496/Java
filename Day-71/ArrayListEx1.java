a6ee267a33227c8312e759b69b29110135f320ac
ReddiRani8496 <reddiranibattuluri@gmail.com>
2023-05-12 08:20:32 +0530

    Day-71 programs done

--- Day-71/ArrayListEx1.java
+++ Day-71/ArrayListEx1.java
@@ -2,18 +2,37 @@ import java.util.*;
 public class ArrayListEx1 {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(110);
+
+        arr.add(110); // adding elements to the ArrayList
+
         arr.add(20);
+        
         arr.add(30);
+        
         arr.add(40);
+        
         arr.add(50);
+        
         arr.add(60);
+        
         arr.add(70);
+        
         arr.add(80);
-        arr.add(90);
-        arr.add(100);
-        System.out.println(arr.get(3));
-        Collections.sort(arr);
+       
+        System.out.println(arr.get(3)); // Accessing an element in the ArrayList
+
+        arr.set(1,120); // modifying an element in the ArrayList
+
+        System.out.println(arr);
+
+        System.out.println(arr.remove(4)); //removing an element in the ArrayList
+
+        System.out.println(arr.size()); // finding the size of the ArrayList
+
+        Collections.sort(arr); // sorting an ArrayList
+
+        System.out.println(arr);
+
         
     }
 }
