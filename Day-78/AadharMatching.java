8d73691afe66421894f6f0a418fb263cef6ca456
reddirani8496 <reddiranibattuluri@gmail.com>
2023-05-20 12:00:18 +0530

    Day-78 programs done

--- Day-78/AadharMatching.java
+++ Day-78/AadharMatching.java
@@ -1,14 +1,15 @@
-// program to check whether the user entered correct aadhar number or not using regural expression.
+// program to check whether the user entered correct aadhar number or not using regular expression.
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.regex.*;;
 public class AadharMatching {
     public static void main(String[] args) {
 
         Pattern aadhar = Pattern.compile("............");
 
         Scanner sc = new Scanner(System.in);
+
+        boolean b1;
 
         Matcher matcher;
 
@@ -18,16 +19,28 @@ public class AadharMatching {
 
             String aadharNum = sc.next();
 
-            if(aadharNum.length() < 12 || aadharNum.length() > 12) {
+             b1 = Pattern.matches("[0-9]{12}",aadharNum);
+
+            matcher = aadhar.matcher(aadharNum);
+            
+            if(!matcher.matches()) {
 
                 System.out.println("The aadhar number must contain 12 digits, try again");
             }
+            else {
 
-            matcher = aadhar.matcher(aadharNum);
+                if(b1) {
 
-        } while (!matcher.matches());
+                    System.out.println("Finally you entered a right number");
 
-        System.out.println("Finally you entered a right number");
+                    break;
+                }
+                else {
+                    System.out.println("Enter valid aadhar number");
+                }
+            }
+
+        } while (!b1);
 
         sc.close();
     }
