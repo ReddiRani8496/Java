b005c71dc2b72e5902b4e5087c251f0e873c5524
reddirani8496 <reddiranibattuluri@gmail.com>
2023-05-24 22:41:08 +0530

    Day-82 programs done

---
+++ Day-82/IsBlankEx.java
@@ -0,0 +1,26 @@
+/*
+ *  The isBlank() method will return true in the below cases:
+
+        If the string is empty.
+
+        If the string only contains whitespaces.
+ */
+public class IsBlankEx {
+    public static void main(String[] args) {
+
+        String first = "  ";
+
+        String last="hello";
+
+        if(first.isEmpty()||first.isBlank()) { // true, first String contains whitespaces
+
+            first="say"; 
+
+            System.out.println(first+" "+last);
+        }
+        
+        String str ="";
+
+        System.out.println(str.isBlank()); // returns true, str String is empty
+    }
+}
