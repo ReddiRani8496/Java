a7a7bce378bfe5835f9244214f3f9b292af15d02
reddirani8496 <reddiranibattuluri@gmail.com>
2023-06-13 20:31:02 +0530

    Day-101 programs done

---
+++ Day-101/BufferedReaderEx.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class BufferedReaderEx {
+    public static void main(String[] args) {
+
+        try{
+
+            FileReader fileReader = new FileReader("Rock.txt");
+
+            BufferedReader br = new BufferedReader(fileReader);
+
+           // System.out.println(br.readLine());// reads the line at a time
+
+            System.out.println(br.markSupported()); // returns true
+            
+            br.mark(3);
+
+            br.reset();
+
+            System.out.println((char)br.read());
+
+            br.close();
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+}
