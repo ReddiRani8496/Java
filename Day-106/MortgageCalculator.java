0d5f10ed3908c833a8b7f514a8a3dbdf6b1acd8e
reddirani8496 <reddiranibattuluri@gmail.com>
2023-06-20 00:07:35 +0530

    Day-106 programs done

--- Day-106/MortgageCalculator.java
+++ Day-106/MortgageCalculator.java
@@ -3,54 +3,92 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MortgageCalculator {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         final byte  MONTHS_IN_YEAR = 12;
+
         final byte PERCENT = 100;
+
         int principal = 0;
+
         float montlyRate = 0;
+
         short numberOfPayments;
 
         while (true) {
-            try{
+
+            try {
+
                 System.out.println("Enter principal: ");
+
                 principal = sc.nextInt();
+
                 if(principal >= 1000 && principal <= 1000000000)
                      break;
-            } catch (InputMismatchException e) {
+
                 System.out.println("Enter amount between 1000 to 1000000000");
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Enter amount between 1000 to 1000000000");
+
                 sc.next();
+
             }
         }
 
         while (true) {
+
             try {
+
                 System.out.print("Annual interest rate : ");
+
                 float yearlyRate = sc.nextFloat();
+
                 if (yearlyRate >= 1 && yearlyRate <= 48) {
+
                     montlyRate = yearlyRate / PERCENT / MONTHS_IN_YEAR;
+
                     break;
                 }
-                else {
+
                     System.out.println("enter interest rate between 1 to 48 percent ");
-                }
+
             } catch (InputMismatchException e) {
+
                 System.out.println("Enter interest rate between 1 to 48 percent ");
+
                 sc.next();
+
             }
         }
         while (true) {
+
             try {
+
                 System.out.print("Time frame (in years): ");
+
                 byte years = sc.nextByte();
+
                 if (years >= 1 && years <= 30) {
+
                     numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
                     break;
+
                 }
+
                 System.out.println("Enter time frame between 1 to 30 years");
-            }catch (Exception e) {
+
+            } catch (Exception e) {
+
                 System.out.println("Enter time frame between 1 to 30 years");
+
                 sc.next();
+
             }
         }
 
