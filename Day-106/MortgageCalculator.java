e7d787bbe733f881870693fcdbdd7f14e7716111
reddirani8496 <reddiranibattuluri@gmail.com>
2023-06-19 21:37:44 +0530

    Day-106 programs done

--- Day-106/MortgageCalculator.java
+++ Day-106/MortgageCalculator.java
@@ -2,7 +2,7 @@ import java.text.NumberFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class MortgageCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         final byte  MONTHS_IN_YEAR = 12;
