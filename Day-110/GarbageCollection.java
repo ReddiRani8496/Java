4f05b71c3d9b9b784e9588f2d08ca4856e83eac9
reddirani8496 <reddiranibattuluri@gmail.com>
2023-06-23 22:48:31 +0530

    Day-110 programs done

---
+++ Day-110/GarbageCollection.java
@@ -0,0 +1,35 @@
+public class GarbageCollection {
+    public static void main(String[] args) {
+        
+        Student s1 = new Student(1, "john");
+        s1.get();
+
+        Student s2 = new Student(2, "king");
+        s2.get();
+
+        Student s3 = new Student(3, "David");
+        s3.get();
+
+        s3=s2; // s3 point to s2 object. It is not possible to point s3 object once it is assigned to another object, we dont't the address of that object
+        s3.get();
+
+    }
+}
+class Student{
+
+    int rollno;
+    String name;
+
+    Student(int rollno,String name) {
+
+        this.rollno=rollno;
+        this.name=name;
+
+    }
+    public void get() {
+
+        System.out.println("rollno: "+rollno);
+        System.out.println("name: "+name);
+        
+    }
+}
