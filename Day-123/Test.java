953f26b1be59ff577570d2954324de9790727430
reddirani8496 <reddiranibattuluri@gmail.com>
2023-07-16 18:04:08 +0530

    Day-123 programs done

--- Day-123/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        char ch=97;
-        System.out.println(ch);
-    }
-    
-}
--- Day-123/Test1.java
+++
@@ -1,9 +0,0 @@
-public class Test1 {
-    public static void main(String[] args) {
-        int x=010;
-        System.out.println(x);
-
-
-    }
-    
-}
