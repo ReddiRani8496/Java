5e0cf037810dab2906dcebcfd2d7132c8aa34afc
reddirani8496 <reddiranibattuluri@gmail.com>
2023-07-20 10:01:44 +0530

    Day-125 programs done

--- Day-125/Time/src/Main.java
+++ Day-125/Time/src/Main.java
@@ -1,8 +1,14 @@
 import time.Time;
 
 public class Main {
+
     public static void main(String[] args) {
-        Time time = new Time(11,60,0);
+
+        Time time = new Time(11,58,30);
         time.display();
+
+        Time time1 = Time.addTwoTimes(new Time(8,90,90),time);
+        time1.display();
+
     }
 }
--- Day-125/Time/src/time/Time.java
+++ Day-125/Time/src/time/Time.java
@@ -1,37 +1,69 @@
 package time;
 
 public class Time {
+
     private int hours;
     private int minutes;
     private int seconds;
+
     public Time(){
+
         hours = minutes = seconds = 0;
+
     }
-    public Time(int hours, int minutes, int seconds){
+    
+    public Time(int hours, int minutes, int seconds) {
+
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
+
+        simplify(seconds);
+
+    }
+
+        public void simplify(int seconds){
+
         if(seconds > 59){
             this.seconds = seconds%60;
             this.minutes += seconds/60;
         }
+
         if(minutes > 59) {
             this.hours += this.minutes / 60;
             this.minutes = this.minutes % 60;
         }
+
         if(this.hours>11) {
             this.hours = this.hours % 12;
         }
+
     }
     public void  display() {
+
         String formatedTime = getFormatedTime();
         System.out.println(formatedTime);
+
     }
 
     public  String getFormatedTime() {
+
         String hour = String.format("%02d", hours);
         String minute = String.format("%02d", minutes);
         String second = String.format("%02d", seconds);
+
         return hour+":"+minute+":"+second;
+
+    }
+
+    public static Time addTwoTimes(Time t1, Time t2) {
+
+        int totalHours = t1.hours+t2.hours;
+        int totalSeconds = t1.seconds+t2.seconds;
+        int totalMinutes = t1.minutes+t2.minutes;
+
+        Time t3 = new Time(totalHours,totalMinutes,totalSeconds);
+        return t3;
+
     }
 }
