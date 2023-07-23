315275fa68625f1a9276b4e1d605adad78bb2f30
reddirani8496 <reddiranibattuluri@gmail.com>
2023-07-23 23:57:38 +0530

    Day-126 programs done

--- Day-126/BankProject2/src/Main.java
+++ Day-126/BankProject2/src/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         BankAccount bankAccount = new BankAccount();
         while (true) {
