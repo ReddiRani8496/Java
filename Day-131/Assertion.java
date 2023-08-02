a3f5d91e987fa23f8ef965f741dd059bc7a6e68f
reddirani8496 <reddiranibattuluri@gmail.com>
2023-08-02 22:29:38 +0530

    Day-131 programs done

---
+++ Day-131/Assertion.java
@@ -0,0 +1,13 @@
+public class Assertion {
+
+    public static void main(String[] args) {
+
+        int age = 14;
+
+        assert age <= 18 : "Not eligible for vote";
+
+        System.out.println("eligible for vote");
+
+    }
+    
+}
