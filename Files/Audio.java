eb7a4d20a64099614b8ea621d5d0b3889a8293f3
reddirani8496 <reddiranibattuluri@gmail.com>
2023-12-19 12:19:05 +0530

    done

---
+++ Files/Audio.java
@@ -0,0 +1,23 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Audio
+{
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("Dog.wav");
+        AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(ais);
+        clip.start(); // start the clip and terminates immediately, doesn't wait until the clip is completed
+
+        for (int i = 0; i < 1000000000; i++) {
+
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next(); // we can make the clip to play and wait until input is taken from user
+
+    }
+}
