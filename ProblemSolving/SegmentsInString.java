4ea52dc96ab24ac3f00b2dcae5cd06a6ccc4ee00
reddirani8496 <reddiranibattuluri@gmail.com>
2024-03-02 12:22:06 +0530

    Segments in a string program done

---
+++ ProblemSolving/SegmentsInString.java
@@ -0,0 +1,15 @@
+public class SegmentsInString {
+    int countSegments(String s) {
+
+        int segments = 0;
+
+        for(int i=0;i<s.length();i++) {
+            if((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                segments++;
+            }
+        }
+
+        return segments;
+    }
+    
+}
