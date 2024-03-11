1f514267de4e4009147e4d31adb220f58e1032f0
reddirani8496 <reddiranibattuluri@gmail.com>
2024-03-11 19:14:23 +0530

    Contains duplicates program done

---
+++ ProblemSolving/ContainsDuplicates.java
@@ -0,0 +1,11 @@
+public class ContainsDuplicates {
+    boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i++) {
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
+    
+}
