8e4d3e48f2eac4f009f1444a048f59652b165d59
reddirani8496 <reddiranibattuluri@gmail.com>
2024-03-12 00:06:51 +0530

    Reverse Letters only program done

---
+++ ProblemSolving/ReverseLettersOnly.java
@@ -0,0 +1,24 @@
+public class ReverseLettersOnly {
+    String reverseOnlyLetters(String s) {
+        int start = 0;
+        int end = s.length()-1;
+        char[] ch = s.toCharArray();
+        while(start < end) {
+            if(!Character.isLetter(ch[start])) {
+                start++;
+            }
+            else if(!Character.isLetter(ch[end])) {
+                end--;
+            }
+            else {
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        return new String(ch);
+    }
+    
+}
