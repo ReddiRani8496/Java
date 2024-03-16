39cab7737d7ecc936d9e2e2f5e7b29f3eada5dfb
reddirani8496 <reddiranibattuluri@gmail.com>
2024-03-16 17:11:01 +0530

    Ugly number program done

---
+++ ProblemSolving/UglyNumber.java
@@ -0,0 +1,15 @@
+public class UglyNumber {
+    public boolean isUgly(int n) {
+        while(n > 1) {
+            if(n % 2 == 0)
+                n /= 2;
+            else if(n % 3 == 0)
+                n /= 3;
+            else if(n % 5 == 0)
+                n /= 5;
+            else
+                break;
+        }
+        return (n==1);
+    }
+}
