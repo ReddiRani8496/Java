4b061d9330830bc6d47b21ef84a1b5078c28c77e
reddirani8496 <reddiranibattuluri@gmail.com>
2024-03-18 07:39:53 +0530

    Prefix of word program done

---
+++ ProblemSolving/PrefixOfWord.java
@@ -0,0 +1,10 @@
+public class PrefixOfWord {
+    int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].startsWith(searchWord))
+                return i+1;
+        }
+        return -1;
+    }
+}
