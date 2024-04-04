86a2ba69410ec4c8a4709b30e8839a2ef2f30736
reddirani8496 <reddiranibattuluri@gmail.com>
2024-04-04 08:33:50 +0530

    Remove all adjacent duplicates program done

---
+++ ProblemSolving/RemoveAllAdjDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class RemoveAllAdjDuplicates {
+        String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
