29e581a6ba3b377d15a9b3c15deb5f204f28085a
reddirani8496 <reddiranibattuluri@gmail.com>
2024-04-06 00:14:13 +0530

    Back space string compare leet code program done

---
+++ ProblemSolving/BackSpaceStringCompare.java
@@ -0,0 +1,21 @@
+public class BackSpaceStringCompare {
+    public boolean backspaceCompare(String s, String t) {
+        return processString(s).equals(processString(t));
+    }
+    
+    private String processString(String str) {
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : str.toCharArray()) {
+            if (c == '#') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1); 
+                }
+            } else {
+                result.append(c); 
+            }
+        }
+        
+        return result.toString();
+    }
+}
