2c94fa7e3d0383eec1bfaebd35bf7b26768afad5
reddirani8496 <reddiranibattuluri@gmail.com>
2024-04-12 22:46:12 +0530

    Tower Of hanoi program updated

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter number of disks: ");
         int disks = sc.nextInt();
+
         towerOfHanoi(disks, "S", "H", "D");
     }
     private static int towerOfHanoi(int disks, String src, String helper, String dest) {
+
         if (disks == 1) {
             System.out.println("Move disk 1 from " + src + " to " + dest);
             return 1;
         }
+        
         int moves = 0;
         moves += towerOfHanoi(disks - 1, src, dest, helper);
         System.out.println("Move disk " + disks + " from " + src + " to " + dest);
