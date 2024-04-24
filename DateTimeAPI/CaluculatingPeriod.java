e02e6399410afe2551fabd15b9af1d9b24fd7d2d
reddirani8496 <reddiranibattuluri@gmail.com>
2024-04-24 19:56:24 +0530

    checking period and duration program uploaded

---
+++ DateTimeAPI/CaluculatingPeriod.java
@@ -0,0 +1,31 @@
+package DateTimeAPI;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Duration;
+import java.time.Period;
+ 
+
+public class CaluculatingPeriod {
+    public static void checkingPeriod() {
+        LocalDate date1 = LocalDate.now();
+        LocalDate date2 = LocalDate.of(2014, Month.DECEMBER, 12);
+        Period gap = Period.between(date2, date1);
+        System.out.println("gap between dates " + "is a period of " + gap);
+    }
+ 
+    public static void checkingDuration() {
+        LocalTime time1 = LocalTime.now();
+        System.out.println("the current time is " + time1);
+        Duration fiveHours = Duration.ofHours(5);
+        LocalTime time2 = time1.plus(fiveHours);
+        System.out.println("after adding five hours " + "of duration " + time2);
+        Duration gap = Duration.between(time2, time1);
+        System.out.println("duration gap between time1" + " & time2 is " + gap);
+    }
+    public static void main(String[] args) {
+        checkingPeriod();
+        checkingDuration();
+    }
+
+}
