b08bfc68f7510411505bb1d988072757aac65d18
reddirani8496 <reddiranibattuluri@gmail.com>
2024-05-17 23:37:23 +0530

    Priority queue program uploaded

---
+++ ProblemSolving/PriorityQueueEx.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+class PriorityQueueEx {
+   
+    public static void main(String args[]){
+       
+        PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
+ 
+        pQueue.add(10);
+        pQueue.add(20);
+        pQueue.add(15);
+ 
+        System.out.println(pQueue.peek());
+ 
+        System.out.println(pQueue.poll());
+ 
+        System.out.println(pQueue.peek());
+    }
+}
